381bc414edb30fbd44e18308ff29c92fade036b4
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-11-11 16:29:53 +0300

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,50 @@
+
+
 class Song
-    attr_accessor :artists, :genre, :name
+    @@count = 0
+    @@artists = []
+    @@genres = []
 
-     @@count=0
-     @@genre_count=0
-     @@artist_count=0
-     @@artists=[]
-     @@genre=[]
-    def initialize(genre,artist,name)
-    @name=name
-    @artist=artist
-    @genre=genre
-    @@artists<<artist
-    @@genres<< genre
-    @@count=+1
-
-    end
     def self.count
         @@count
     end
-    def genres
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
         @@genres.uniq
     end
-    def self.genre_count
-        @@genres.tally
+    def self.genre_count 
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
     end
-    def self.artists_count
-        @@artists.tally
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            if artist_count[artist]
+                artist_count[artist] += 1
+            else
+                artist_count[artist] = 1
+            end
+        end
+        artist_count
+    end
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
     end
 end
